287c4e8fc91c22da251c2f571d28b0d46ae02048
kirychukyurii <yurii.kirychuk.knz.2017@lpnu.ua>
2021-03-17 16:11:54 +0200

    Revert disabled check certificate

--- controller/tool/mail.php
+++ controller/tool/mail.php
@@ -24,13 +24,7 @@ class ControllerToolMail extends Controller
 
     echo " " . sprintf($this->language->get('text_send_mail'), $data['to']);
     $mail = new Mail($this->config->get('config_mail_engine'));
-    $mail->parameter = $this->config->array(
-        'ssl' => array(
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-            'allow_self_signed' => true
-        )
-    );
+    $mail->parameter = $this->config->get('config_mail_parameter');
     $mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
     $mail->smtp_username = $this->config->get('config_mail_smtp_username');
     $mail->smtp_password = html_entity_decode($this->config->get('config_mail_smtp_password'), ENT_QUOTES, 'UTF-8');
